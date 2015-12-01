0f84c0f0e2ba22d121d484ed25aff009f861b015
LuisEnVilla <villak@hotmail.es>
2015-11-30 21:51:08 -0600

    Frame create

--- index.js
+++ index.js
@@ -1,44 +1,86 @@
 #!/usr/bin/env node
+
 var optimist = require('optimist');
 var net = require('net');
+var readline = require('readline');
+var moment = require('moment');
+var rl = readline.createInterface(process.stdin, process.stdout);
+var datGL200 = {
+	protocolVersion: '020102',
+	uniqueID: '123456789123654',
+	deviceName: '',
+	number: '5217721199947',
+	speed: '127.5',
+	azimuth: '123',
+	altitude: '340',
+	longitude: '-98.707299',
+	latitude: '20.102827',
+	mcc: '0334',
+	mnc: '0020',
+	lac: '1234',
+	cellId: '1234',
+	reserved: '00',
+	battery: '100',
+	count: 'FFFF'
+}
 
-var DOG_TRACKER_LOGIN = "(013500001112BP05000013500001112120903A5956.1894N01046.9892E006.0160134061.9600000000L00000000)";
-var DOG_TRACKER_ISOCHRONOUS_FOR_CONTINUES_FEEDBACK_MESSAGE = "(013500001112BR00120903A5955.9535N01047.4548E000.0160957000.0000000000L00000000)"
+rl.setPrompt('GL200>> ');
 
+var argv = optimist.usage('Usage: $0 --host [NUMBER] -port [NUMBER]').
+options('h', {
+	alias: 'host',
+	describe: 'server host',
+	default: '127.0.0.1'
+}).
+options('p', {
+	alias: 'port',
+	describe: 'server port',
+	default: '8090'
+}).
+argv;
 
-var argv = optimist.usage('Usage: $0 --interval [num] -serial [num] -lat [latitude] -lng [longitude]').
-    options('i', {
-        alias : 'interval',
-        default : '30',
-        describe: 'update interval'
-    }).
-    options('s', {
-        alias : 'serial',
-        default : '',
-        describe: '013612345678'
-    }).
-    options('x', {
-        alias : 'lat',
-        default : '52.80113',
-        describe: 'initial latitude'
-    }).
-    options('y', {
-        alias : 'lng',
-        default: '-1.63130',
-        describe: 'initial longitude'
-    }).
-    options('h', {
-        alias : 'host',
-        describe: 'server host'
-    }).
-    options('p', {
-        alias : 'port',
-        describe: 'server port',
-        default: '1337'
-    }).
-    argv;
-
-var client = net.connect({host:argv.host, port:argv.port }, function() {
-     client.write(DOG_TRACKER_LOGIN);
-     console.log('login sent');
+var client = net.connect({
+	host: argv.host,
+	port: argv.port
+}, function () {
+	console.log('         IP: ' + argv.host + '  Port:' + argv.port);
+	console.log('=======================================');
+	console.log('             GL200-Generator');
+	console.log('=======================================\n\n');
+	console.log('------General Posotion Report\n');
+	console.log('/sos --- GTSOS\n\n');
+	rl.prompt();
+	rl.on('line', function (line) {
+		switch (line.trim()) {
+		case '/sos':
+			var frame = '+RESP:GTSOS,' +
+				datGL200.protocolVersion + ',' +
+				datGL200.uniqueID + ',,0,0,' +
+				datGL200.number + ',20,' +
+				datGL200.speed + ',' +
+				datGL200.azimuth + ',' +
+				datGL200.altitude + ',' +
+				datGL200.longitude + ',' +
+				datGL200.latitude + ',' +
+				moment().format('YYYYMMDDHHmmss') + ',' +
+				datGL200.mcc + ',' +
+				datGL200.mnc + ',' +
+				datGL200.lac + ',' +
+				datGL200.cellId + ',' +
+				datGL200.reserved + ',' +
+				datGL200.battery + ',' +
+				moment().format('YYYYMMDDHHmmss') + ',' +
+				datGL200.count + '$'
+			console.log(frame)
+				//client.write(DOG_TRACKER_LOGIN);
+			break;
+		default:
+			console.log('Comand Error');
+			break;
+		}
+		rl.prompt();
+	}).on('close', function () {
+		console.log('---- Socket Disconnect');
+		process.exit(0);
+	});
 });
